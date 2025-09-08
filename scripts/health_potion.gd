8f3a6616cfcb3cb3291fc13f229fb42736730ce6
budopod1 <51675874+budopod1@users.noreply.github.com>
2025-09-08 13:25:51 -0700

    Added: health potion

--- scripts/health_potion.gd
+++ scripts/health_potion.gd
@@ -6,30 +6,30 @@ class_name HealthPotion
 @export var auto_pickup: bool = true
 
 func _ready():
-    print("Health Potion created - heals " + str(heal_amount) + " HP")
-    # Connect the collision signal
-    body_entered.connect(_on_body_entered)
+	print("Health Potion created - heals " + str(heal_amount) + " HP")
+	# Connect the collision signal
+	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
-    # Check if it's the player
-    if body is Player:
-        print("Player found health potion!")
-        
-        # Try to heal the player (will work once they add heal method)
-        if body.has_method("heal"):
-            body.heal(heal_amount)
-            print("Player healed for " + str(heal_amount) + " HP!")
-            
-            # Remove the potion after use
-            if auto_pickup:
-                queue_free()
-        else:
-            print("Player doesn't have heal() method yet - coming in Lesson 2!")
+	# Check if it's the player
+	if body is Player:
+		print("Player found health potion!")
+		
+		# Try to heal the player (will work once they add heal method)
+		if body.has_method("heal"):
+			body.heal(heal_amount)
+			print("Player healed for " + str(heal_amount) + " HP!")
+			
+			# Remove the potion after use
+			if auto_pickup:
+				$AnimationPlayer.play("disappear")
+		else:
+			print("Player doesn't have heal() method yet - coming in Lesson 2!")
 
 func use_potion(player: Player):
-    """Alternative method for manual potion use"""
-    if player.has_method("heal"):
-        player.heal(heal_amount)
-        queue_free()
-        return true
-    return false
+	"""Alternative method for manual potion use"""
+	if player.has_method("heal"):
+		player.heal(heal_amount)
+		$AnimationPlayer.play("disappear")
+		return true
+	return false
