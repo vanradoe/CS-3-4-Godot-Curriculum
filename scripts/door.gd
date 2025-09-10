740459414f5dc6ad0c17f66fb61bb6f92a590a27
Inocybe <140019717+Inocybe@users.noreply.github.com>
2025-09-10 13:36:23 -0700

    fixed lighting and added door to scene

--- scripts/door.gd
+++ scripts/door.gd
@@ -5,6 +5,8 @@ func set_is_open(is_open: bool) -> void:
 	if is_open:
 		$AnimatedSprite2D.frame = 0
 		collision_layer = 1
+		$LightOccluder2D.visible = false
 	else:
 		$AnimatedSprite2D.frame = 1
 		collision_layer = 0
+		$LightOccluder2D.visible = true
