9b61c628528cd9d0f83014206109d34895d10381
vanradoe <viviknight8@gmail.com>
2025-09-17 12:28:28 -0700

    remove coin after pickup, coin value added.

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -7,27 +7,38 @@ class_name Coin
 
 
 @export var color: Color
-@export var coin_value: int = 5
+@export var coin_value: int = 50
 @export var auto_pickup: bool = true
 
 func _ready():
-	print("Coin created - worth 1 Gold" + str(coin_value) + " Gold")
+	print("Coin created - worth " + str(coin_value) + " Gold")
 	# Connect the collision signal
 	body_entered.connect(_on_body_entered)
+
 
 
 func _on_body_entered(body):
 	# Check if it's the player
 	if body is Player:
-		print("Player found 1 Gold!") 
+		print("Player found " + str(coin_value) + " Gold!") 
+		
+		if body.has_method("collect"):
+			body.collect(coin_value)
+			print("Player used " + str(coin_value) + " Gold!")
+			
+			# Remove the potion after use
+			if auto_pickup:
+				$AnimationPlayer.play("disappear")
+		else:
+			print("Player doesn't have collect() method yet - coming soon!")
 
-#func spend_coin (player: Player):
-	#"""Alternative method for manual coin use"""
-	#if player.has_method("spend"):
-		#player.spend(spend_amount)
-		#$AnimationPlayer.play("disappear")
-		#return true
-	#return false
+func collect_coin (player: Player):
+	"""Alternative method for manual coin use"""
+	if player.has_method("collect"):
+		player.collect(coin_value)
+		$AnimationPlayer.play("disappear")
+		return true
+	return false
 
 
 func _process(delta: float) -> void:
