36ee3ed41ff4a34102b2718f17d936206f6036ae
vanradoe <viviknight8@gmail.com>
2025-09-25 14:03:44 -0700

    added some more of the npc code
    tried to separate the npc from the player - safe to say, it hasn't worked yet.

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 class_name npc
 
 @export var dialogue: Array[String]
+@export var inventory: Array[String]
 @export var speed: int = 200
 @export var direction: Vector2 = Vector2.DOWN
 @export var health: int = 10
@@ -9,12 +10,31 @@ class_name npc
 @export var ishostile: bool = false
 @export var type: String
 @export var move_points: Array[Vector2] = []
+@export var drop_item = preload("res://scenes/coin.tscn")
+@export var drop_rate: float = 1.0
+@export var state = 0
 
 func _ready():
 	pass
 	
-func _physics_process(_delta):
+func _process(_delta):
 	pass
+	
+func _drop_item():
+	#create a new copy of the prefab object
+	var drop = drop_item.instantiate()
+	
+	#child drop to the parent scene
+	get_tree().get_root().add_child(drop)
+	
+	#set the in game position to the position of the npc
+	drop.global_position = position
 
-func movement():
-	pass
+func movement(_delta):
+	if ishostile:
+		position = position.move_toward(Vector2)
+	else:
+		move_points[-8,20]
+
+
+#CONNECT NPC SPRITE TO SCRIPTS. without having it do the same things as the main player.
