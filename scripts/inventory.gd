3f19adf11752b8470cc6a07f9764acba71ebed02
vanradoe <viviknight8@gmail.com>
2025-10-24 13:49:22 -0700

    added more to the merchent

--- scripts/inventory.gd
+++ scripts/inventory.gd
@@ -1,7 +1,34 @@
 extends Node
 class_name inventory
+var selected_item
+var max_inventory = 5
 
-inventory[inventory_items]
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("item_up"):
+		selected_item -=1
+		if selected_item <0:
+			selected_item = 0
+		print(inventory[selected_item])
+	
+	if Input.is_action_just_pressed("item_down"):
+		selected_item +=1
+		if selected_item >inventory.size()-1:
+			selected_item = inventory.size()-1
+		print(inventory[selected_item])
+
+
+func show_wares():
+	print("here is my inventory")
+	#for item in inventory:
+		#print(item)
+	print(inventory[0])
+
+
+func show_wares():
+	print("here is my inventory")
+	#for item in inventory:
+		#print(item)
+	print(inventory[0])
 
 #print specific inventory
 	#print type
--- scripts/merchant.gd
+++ scripts/merchant.gd
@@ -11,25 +11,7 @@ var greeting = "welcome to my shop."
 var selected_item = 0
 
 
-func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("item_up"):
-		selected_item -=1
-		if selected_item <0:
-			selected_item = 0
-		print(inventory[selected_item])
-	
-	if Input.is_action_just_pressed("item_down"):
-		selected_item +=1
-		if selected_item >inventory.size()-1:
-			selected_item = inventory.size()-1
-		print(inventory[selected_item])
 
-
-func show_wares():
-	print("here is my inventory")
-	#for item in inventory:
-		#print(item)
-	print(inventory[0])
 	
 func _on_detection_radius_body_entered(_body: Node2D) -> void:
 	super._on_detection_radius_body_entered(_body)
